d49344c18434fb988c7f14dc154f38845360fe7c
Amal-mm <124715272+Amal-mm@users.noreply.github.com>
2025-04-30 19:06:46 +0300

    Add files via upload
    Welcome page

---
+++ welcomPage.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Amal9909.demo3
+{
+    public partial class welcomPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
