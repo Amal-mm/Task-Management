be2a9ae82ec12cd40b6a4e96716e7ed59773c18b
Amal-mm <124715272+Amal-mm@users.noreply.github.com>
2025-04-30 19:07:15 +0300

    Add files via upload

---
+++ Repeator.aspx.cs
@@ -0,0 +1,33 @@
+﻿using Amal9909.App_Code;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Amal9909.demo3
+{
+    public partial class Repeator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadTasks();
+        }
+        protected void LoadTasks()
+        {
+            CRUD myCrud = new CRUD();
+            string mySql = @"SELECT * FROM v_TaskInfo";
+            SqlDataReader dr = myCrud.getDrPassSql(mySql);
+
+            rptTasks.DataSource = dr;
+            rptTasks.DataBind();
+        }
+
+        protected void rptTasks_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+
+        }
+    }
+}
